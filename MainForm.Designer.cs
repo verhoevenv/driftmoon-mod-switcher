a0cd4d8c1c0948ec63c274091857f618cb0ed1be
Vincent Verhoeven <verhoevenv@gmail.com>
2010-06-19 23:33:59 +0200

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿/*
+Copyright 2010 Vincent Verhoeven
+
+This file is part of driftmoon-mod-switcher.
+
+driftmoon-mod-switcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+driftmoon-mod-switcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with driftmoon-mod-switcher.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace driftmoon_mod_switcher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InstalledLB = new System.Windows.Forms.ListBox();
+            this.InstalledL = new System.Windows.Forms.Label();
+            this.InstallDirL = new System.Windows.Forms.Label();
+            this.InstallDirT = new System.Windows.Forms.TextBox();
+            this.InstallDirB = new System.Windows.Forms.Button();
+            this.CurrentModL = new System.Windows.Forms.Label();
+            this.CurrentModT = new System.Windows.Forms.TextBox();
+            this.CurrentModB = new System.Windows.Forms.Button();
+            this.InstallFromZipB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InstalledLB
+            // 
+            this.InstalledLB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.InstalledLB.ColumnWidth = 20;
+            this.InstalledLB.FormattingEnabled = true;
+            this.InstalledLB.Location = new System.Drawing.Point(12, 50);
+            this.InstalledLB.Name = "InstalledLB";
+            this.InstalledLB.Size = new System.Drawing.Size(439, 134);
+            this.InstalledLB.TabIndex = 0;
+            // 
+            // InstalledL
+            // 
+            this.InstalledL.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.InstalledL.AutoSize = true;
+            this.InstalledL.Location = new System.Drawing.Point(177, 34);
+            this.InstalledL.Name = "InstalledL";
+            this.InstalledL.Size = new System.Drawing.Size(104, 13);
+            this.InstalledL.TabIndex = 1;
+            this.InstalledL.Text = "Installed mods found";
+            // 
+            // InstallDirL
+            // 
+            this.InstallDirL.AutoSize = true;
+            this.InstallDirL.Location = new System.Drawing.Point(9, 14);
+            this.InstallDirL.Name = "InstallDirL";
+            this.InstallDirL.Size = new System.Drawing.Size(124, 13);
+            this.InstallDirL.TabIndex = 2;
+            this.InstallDirL.Text = "Driftmoon install directory";
+            // 
+            // InstallDirT
+            // 
+            this.InstallDirT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.InstallDirT.Enabled = false;
+            this.InstallDirT.Location = new System.Drawing.Point(139, 11);
+            this.InstallDirT.Name = "InstallDirT";
+            this.InstallDirT.Size = new System.Drawing.Size(231, 20);
+            this.InstallDirT.TabIndex = 3;
+            // 
+            // InstallDirB
+            // 
+            this.InstallDirB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.InstallDirB.Location = new System.Drawing.Point(376, 9);
+            this.InstallDirB.Name = "InstallDirB";
+            this.InstallDirB.Size = new System.Drawing.Size(75, 23);
+            this.InstallDirB.TabIndex = 4;
+            this.InstallDirB.Text = "Change";
+            this.InstallDirB.UseVisualStyleBackColor = true;
+            this.InstallDirB.Click += new System.EventHandler(this.InstallDirB_Click);
+            // 
+            // CurrentModL
+            // 
+            this.CurrentModL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.CurrentModL.AutoSize = true;
+            this.CurrentModL.Location = new System.Drawing.Point(12, 207);
+            this.CurrentModL.Name = "CurrentModL";
+            this.CurrentModL.Size = new System.Drawing.Size(67, 13);
+            this.CurrentModL.TabIndex = 5;
+            this.CurrentModL.Text = "Current mod:";
+            // 
+            // CurrentModT
+            // 
+            this.CurrentModT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.CurrentModT.Enabled = false;
+            this.CurrentModT.Location = new System.Drawing.Point(85, 204);
+            this.CurrentModT.Name = "CurrentModT";
+            this.CurrentModT.Size = new System.Drawing.Size(243, 20);
+            this.CurrentModT.TabIndex = 6;
+            // 
+            // CurrentModB
+            // 
+            this.CurrentModB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CurrentModB.Location = new System.Drawing.Point(334, 202);
+            this.CurrentModB.Name = "CurrentModB";
+            this.CurrentModB.Size = new System.Drawing.Size(117, 23);
+            this.CurrentModB.TabIndex = 7;
+            this.CurrentModB.Text = "Use selected";
+            this.CurrentModB.UseVisualStyleBackColor = true;
+            this.CurrentModB.Click += new System.EventHandler(this.CurrentModB_Click);
+            // 
+            // InstallFromZipB
+            // 
+            this.InstallFromZipB.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.InstallFromZipB.Location = new System.Drawing.Point(132, 230);
+            this.InstallFromZipB.Name = "InstallFromZipB";
+            this.InstallFromZipB.Size = new System.Drawing.Size(194, 23);
+            this.InstallFromZipB.TabIndex = 8;
+            this.InstallFromZipB.Text = "Install downloaded mod from directory";
+            this.InstallFromZipB.UseVisualStyleBackColor = true;
+            this.InstallFromZipB.Click += new System.EventHandler(this.InstallModB_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 265);
+            this.Controls.Add(this.InstallFromZipB);
+            this.Controls.Add(this.CurrentModB);
+            this.Controls.Add(this.CurrentModT);
+            this.Controls.Add(this.CurrentModL);
+            this.Controls.Add(this.InstallDirB);
+            this.Controls.Add(this.InstallDirT);
+            this.Controls.Add(this.InstallDirL);
+            this.Controls.Add(this.InstalledL);
+            this.Controls.Add(this.InstalledLB);
+            this.Name = "MainForm";
+            this.Text = "Driftmoon Mod Switcher";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox InstalledLB;
+        private System.Windows.Forms.Label InstalledL;
+        private System.Windows.Forms.Label InstallDirL;
+        private System.Windows.Forms.TextBox InstallDirT;
+        private System.Windows.Forms.Button InstallDirB;
+        private System.Windows.Forms.Label CurrentModL;
+        private System.Windows.Forms.TextBox CurrentModT;
+        private System.Windows.Forms.Button CurrentModB;
+        private System.Windows.Forms.Button InstallFromZipB;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,224 @@
+﻿/*
+Copyright 2010 Vincent Verhoeven
+
+This file is part of driftmoon-mod-switcher.
+
+driftmoon-mod-switcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+driftmoon-mod-switcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with driftmoon-mod-switcher.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+
+namespace driftmoon_mod_switcher {
+    public partial class MainForm : Form {
+        private Regex modPattern = new Regex("^Mod=(\\w*)", RegexOptions.Multiline);
+        private Boolean settingsChanged = false;
+
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            try {
+                loadSettings();
+            } catch (ApplicationException) {
+                findDriftmoonDir();
+                settingsChanged = true;
+            }
+        }
+
+        private void loadSettings() {
+            try {
+                TextReader tr = new StreamReader(Application.UserAppDataPath + "\\driftmoonpath.txt");
+                string driftmoondir = tr.ReadLine();
+                tr.Close();
+                if (!isDriftmoonDir(driftmoondir))
+                    throw new ApplicationException("No valid settings found");
+                setDriftmoonDir(driftmoondir);
+            } catch (IOException) {
+                throw new ApplicationException("No settings found");
+            }
+        }
+
+        private void saveSettings() {
+            if (!settingsChanged)
+                return;
+            TextWriter tw = new StreamWriter(Application.UserAppDataPath + "\\driftmoonpath.txt");
+            tw.WriteLine(InstallDirT.Text);
+            tw.Close();
+        }
+
+        private void refreshMods() {
+            refreshModList();
+            refreshCurrentMod();
+        }
+
+        private void refreshCurrentMod() {
+            TextReader tr = new StreamReader(InstallDirT.Text + "\\options.ini");
+            string options = tr.ReadToEnd();
+            tr.Close();
+            Match m = modPattern.Match(options);
+            string mod = m.Groups[1].Value;
+            Match m2 = m.NextMatch();
+            if (m2.Success) {
+                MessageBox.Show("There are multiple mods defined in options.ini, please fix this before I break something. I am clumsy.",
+                        "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            CurrentModT.Text = mod;
+            InstalledLB.SelectedItem = mod;
+        }
+
+        private void refreshModList() {
+            string[] dirs = Directory.GetDirectories(InstallDirT.Text);
+            List<string> mods = new List<string>();
+            foreach (string dir in dirs) {
+                string lastpart = dir.Substring(dir.LastIndexOf("\\") + 1);
+                if (lastpart != "ui" && lastpart != "data") {
+                    mods.Add(lastpart);
+                }
+            }
+            InstalledLB.Items.Clear();
+            InstalledLB.Items.AddRange(mods.ToArray());
+        }
+
+        private void findDriftmoonDir() {
+            string d = ProgramFilesx86() + "\\Driftmoon";
+            setDriftmoonDir(d);
+        }
+
+        private void setDriftmoonDir(string d){
+            if (isDriftmoonDir(d)) {
+                InstallDirT.Text = d;
+                refreshMods();
+            }
+        }
+
+        private bool isDriftmoonDir(string path) {
+            return Directory.Exists(path) && File.Exists(path + "\\Driftmoon.exe")
+                && File.Exists(path + "\\options.ini");
+        }
+
+        private void InstallDirB_Click(object sender, EventArgs e) {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            f.Description = "Select the Driftmoon install directory";
+            f.ShowNewFolderButton = false;
+            DialogResult result = f.ShowDialog();
+            if (result == DialogResult.OK) {
+                string d = f.SelectedPath;
+                if (isDriftmoonDir(d)) {
+                    settingsChanged = true;
+                    InstallDirT.Text = d;
+                    refreshMods();
+                } else {
+                    MessageBox.Show("This is not a valid Driftmoon install directory!",
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void CurrentModB_Click(object sender, EventArgs e) {
+            if (InstalledLB.SelectedItem == null)
+                return;
+            string newMod = (string)InstalledLB.SelectedItem;
+            if (CurrentModT.Text == newMod)
+                return;
+
+            changeMod(newMod);
+
+            refreshCurrentMod();
+        }
+
+        private void changeMod(string newMod) {
+            TextReader tr = new StreamReader(InstallDirT.Text + "\\options.ini");
+            string options = tr.ReadToEnd();
+            tr.Close();
+            string[] lines = options.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            try {
+                TextWriter tw = new StreamWriter(InstallDirT.Text + "\\options.ini");
+                foreach (string line in lines) {
+                    Match m = modPattern.Match(line);
+                    if (m.Success) {
+                        tw.WriteLine("Mod=" + newMod);
+                    } else {
+                        tw.WriteLine(line);
+                    }
+                }
+                tw.Close();
+            } catch (UnauthorizedAccessException ex) {
+                MessageBox.Show("Windows told me: \"" + ex.Message + "\" Perhaps try running as administrator?", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void InstallModB_Click(object sender, EventArgs e) {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            f.Description = "Select the extracted mod directory to install";
+            f.ShowNewFolderButton = false;
+            DialogResult result = f.ShowDialog();
+            if (result == DialogResult.OK) {
+                string d = f.SelectedPath;
+                try {
+                    DirectoryCopy(d, InstallDirT.Text + d.Substring(d.LastIndexOf("\\")));
+                    changeMod(d.Substring(d.LastIndexOf("\\") + 1));
+                    refreshMods();
+                } catch (UnauthorizedAccessException ex) {
+                    MessageBox.Show("Windows told me: \"" + ex.Message + "\" Perhaps try running as administrator?", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private static void DirectoryCopy(
+            string sourceDirName, string destDirName) {
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!Directory.Exists(destDirName)) {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files) {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            foreach (DirectoryInfo subdir in dirs) {
+                string temppath = Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, temppath);
+            }
+        }
+
+        static string ProgramFilesx86() {
+            if (8 == IntPtr.Size
+                || (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432")))) {
+                return Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+            }
+
+            return Environment.GetEnvironmentVariable("ProgramFiles");
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e) {
+            saveSettings();
+        }
+
+    }
+}
